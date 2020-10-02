422aefd65685e404fb645a9ce4274f0b25510f93
Braxton Martin <braxton_martin@users.noreply.github.com>
2020-10-02 09:08:59 -0400

    NA

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,20 +1,26 @@
 import java.util.*;
 public class AddressBook {
-    ArrayList<BuddyInfo> list;
+    ArrayList<BuddyInfo> myBuddies;
 
-    public void addBuddy(BuddyInfo buddy) {
-        list.add(buddy);
+    public AddressBook(){
+        myBuddies = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo aBuddy) {
+        if(aBuddy!= null) myBuddies.add(aBuddy);
     }
 
-    public void removeBuddy(BuddyInfo buddy){
-        list.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+        if(index>=0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo("tom", "Carleton", "613");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressbook = new AddressBook();
         addressbook.addBuddy(buddy);
-        addressbook.removeBuddy(buddy);
+        addressbook.removeBuddy(0);
     }
 }
 
